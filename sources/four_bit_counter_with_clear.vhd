03d15afa1948c9220456db3e2807c9bca5111320
Randipa Gunathilake <wrg20@uclive.ac.nz>
2020-05-13 11:48:46 +1200

    Quad counter added

--- sources/four_bit_counter_with_clear.vhd
+++ sources/four_bit_counter_with_clear.vhd
@@ -1,6 +1,4 @@
 library IEEE;
--- As discussed in Lecture 8 of ENEL373, 2019
--- ------------------------------------------
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all; -- this is needed for the adder
 -- i.e., the "+"
@@ -8,37 +6,40 @@ use ieee.std_logic_unsigned.all; -- this is needed for the adder
 -- external (to the FPGA) input and output connections
 entity counter_4bit is
 	port (
+	    EN : in std_logic := '1';
 		Clock : in std_logic;
 		CLR : in std_logic := '0';
 		clk_out : out std_logic;
 		Q : out std_logic_vector(3 downto 0)
-	);
+		);
 end counter_4bit;
  
 architecture behav of counter_4bit is 
 	constant clk_limit : std_logic_vector(3 downto 0) := X"9";
-	signal tmp : std_logic_vector(3 downto 0); -- internal to FPGA
+	signal tmp : std_logic_vector(3 downto 0) := "0000"; -- internal to FPGA
 	-- note, "in" or "out" 
 	-- are invalid
 	-- you can also define components here, and only here.
  
 begin
-	process (Clock, CLR) -- if either of these signals change,
+	process (Clock, CLR, EN) -- if either of these signals change,
 	-- run this process
 	begin
 		if (CLR = '1') then -- this is an asynchronous clear
 			tmp <= "0000"; 
-		elsif (Clock'EVENT and Clock = '1') then
-			if tmp = clk_limit then
-				tmp <= "0000";
-				clk_out <= '1'; 
-			else
-				clk_out <= '0';
-				tmp <= tmp + 1; -- Counters uses flip-flops to
-				-- "remember" the last count,
-				-- which can then be added to.
-			end if;
-		end if; 
+		elsif (EN = '1') then
+			if (Clock'EVENT and Clock = '1') then
+				if tmp = clk_limit then
+					tmp <= "0000";
+					clk_out <= '1'; 
+				else
+						clk_out <= '0';
+						tmp <= tmp + 1; -- Counters uses flip-flops to
+						-- "remember" the last count,
+						-- which can then be added to.
+				end if;
+			end if; 
+		end if;
  
 	end process;
  
--- sources/quad_4_bit_counter.vhd
+++ sources/quad_4_bit_counter.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: UC
+-- Engineer: Jeremy
 -- 
 -- Create Date: 11.05.2020 11:03:04
 -- Design Name: 
@@ -20,44 +20,50 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+--TODO: Do we need these?
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 entity quad_4_bit_counter is
-    port ( EN : in STD_LOGIC := '1';
-          R_SET : in STD_LOGIC := '0';
-          stage_1_q_out : out STD_LOGIC_VECTOR (3 downto 0);
-          stage_2_q_out : out STD_LOGIC_VECTOR (3 downto 0);
-          stage_3_q_out : out STD_LOGIC_VECTOR (3 downto 0);
-          stage_4_q_out : out STD_LOGIC_VECTOR (3 downto 0);
-          clk_in_ctr : in STD_LOGIC;
-          overflow : out STD_LOGIC);
+    Port (  EN : in STD_LOGIC := '1';
+            R_SET : in STD_LOGIC := '0';
+            stage_1_q_out : out STD_LOGIC_VECTOR (3 downto 0);
+            stage_2_q_out : out STD_LOGIC_VECTOR (3 downto 0);
+            stage_3_q_out : out STD_LOGIC_VECTOR (3 downto 0);
+            stage_4_q_out : out STD_LOGIC_VECTOR (3 downto 0);
+            clk_in_ctr : in STD_LOGIC;
+            overflow : out STD_LOGIC);
 end quad_4_bit_counter;
 
 architecture Behavioral of quad_4_bit_counter is
 
-signal cnt0_1, cnt1_2, cnt2_3, cnt3_4: std_logic;
-
 component counter_4bit
 	port (
+	    EN : in std_logic := '1';
 		Clock : in std_logic;
 		CLR : in std_logic := '0';
 		clk_out : out std_logic;
 		Q : out std_logic_vector(3 downto 0));
 end component;
 
+signal cnt0_1, cnt1_2, cnt2_3 : std_logic;
+
 begin
-    cnt0: counter_4bit port map(Clock=>clk_in_ctr,
-    CLR=>open, clk_out=>cnt0_1,
+    cnt0: counter_4bit port map(EN => EN, Clock=>clk_in_ctr,
+    CLR=>R_SET, clk_out=>cnt0_1,
     Q=>stage_1_q_out);
     
-    cnt1: counter_4bit port map(Clock=>cnt0_1,
-    CLR=>open, clk_out=>cnt1_2,
+    cnt1: counter_4bit port map(EN => EN, Clock=>cnt0_1,
+    CLR=>R_SET, clk_out=>cnt1_2,
     Q=>stage_2_q_out);
     
-    cnt2: counter_4bit port map(Clock=>cnt1_2,
-    CLR=>open, clk_out=>cnt2_3,
+    cnt2: counter_4bit port map(EN => EN, Clock=>cnt1_2,
+    CLR=>R_SET, clk_out=>cnt2_3,
     Q=>stage_3_q_out);
     
-    cnt3: counter_4bit port map(Clock=>cnt2_3,
-    CLR=>open, clk_out=>cnt3_4,
+    --TODO: Overflow 
+    cnt3: counter_4bit port map(EN => EN, Clock=>cnt2_3,
+    CLR=>R_SET, clk_out=>overflow,
     Q=>stage_4_q_out);
 
 end Behavioral;
